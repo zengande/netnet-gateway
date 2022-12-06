c7416ef917bfcc8ef6542b42524930a577ae3fa6
ande zeng <zengande@outlook.com>
2022-12-06 21:54:10 +0800

    feat: 字典输入框

--- src/NetNet.Gateway.Admin/Components/DictionaryInput.razor.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Components;
-
-namespace NetNet.Gateway.Admin.Components;
-
-public partial class DictionaryInput<TKey, TValue> where TKey : notnull
-{
-    [Parameter]
-    public Dictionary<TKey, TValue>? Data { get; set; }
-}
